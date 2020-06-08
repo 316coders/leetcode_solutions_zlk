16de5a52e6b17ef2f3fee37dfece0a0fc693a600
zhangliukun <1433317488@qq.com>
2020-06-08 14:59:43 +0800

    updata:  344  704

--- main.cpp
+++ main.cpp
@@ -1,48 +1,88 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+/**
+ * 题目描述：
+设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。
+每次调用 KthLargest.add，返回当前数据流中第K大的元素。
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+
+**/
+
+/**
+ * 题解：
+ * 方法一：直接使用C++自带小根堆来解决
+    第k大/小的元素。首先想到的就是堆。
+    堆有小根堆和大根堆。
+    大根堆：priority_queue<int, vector<int>, less<int> > q;或者priority_queue<int> q;
+    小根堆：priority_queue<int, vector<int>, greater<int> > q;
+    priority_queue<Type, Container, Functional>
+    Type为数据类型， Container为保存数据的容器，Functional为元素比较方式。
+    如果不写后两个参数，那么容器默认用的是vector，比较方式默认用operator<，也就是优先队列是大顶堆，队头元素最大。
+
+解题思路：
+一. 将数组nums[]中的元素从minHeap[1]开始依次放入，这里分为 num.length >= k 和 num.length < k 两种情况
+1. num.length >= k：
+- 将num中的前个元素，依次放入堆中
+- 从第k+1个元素开始，对元素进行add()操作
+2. num.length < k：
+- 将num中的前个元素，依次放入堆中
+二. add()会判断是 num.length >= k 和 num.length < k 哪种情况
+1. num.length >= k
+- 此时堆为有序的，堆顶（即minHeap[1]），为堆中最小元素，同时也是所有数中第k大元素
+- 若新元素>堆顶元素，则替换对顶元素，并对堆顶元素进行sink()（下沉操作），若新元素<=堆顶元素,则丢弃不作操作
+2. num.length < k
+- 由题nums.length >= k-1，所以当进行一次add()操作后堆会被填满
+- 填满后对堆进行有序化
+
+ * 方法二：使用multiset，利用它的自动排序
+
+
+ */
+
+class KthLargest {
+private:
+    priority_queue<int,vector<int>, greater<int>> p;
+    int K;
 public:
-    int search(vector<int>& nums, int target) {
-        int start=0,mid=0, end = nums.size();
-        while (start < end){
-            mid = start + (end - start) / 2; //这样算可以防止right+left可能会溢出的问题
-            if(target < nums[mid]){
-                end = mid;
-            }else if(target > nums[mid]){
-                start = mid + 1;
-            } else{
-                return mid;
+    KthLargest(int k, vector<int>& nums) {
+        for(int n:nums){
+            p.push(n);
+            if(p.size() > k){ //大于k的话则弹出最大的值
+                p.pop();
             }
         }
-        return -1;
+        K = k;
     }
-    int search2(vector<int>& nums, int target) {
-        int pivot, left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            pivot = left + (right - left) / 2;
-            if (nums[pivot] == target) return pivot;
-            if (target < nums[pivot]) right = pivot - 1;
-            else left = pivot + 1;
+
+    int add(int val) {
+        p.push(val);
+        if(p.size() > K){
+            p.pop();
         }
-        return -1;
+        return p.top();
     }
 };
 
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
 
 int main() {
 
-    vector<int> obj;
-    for (int i=0;i<10;i++){
-        obj.push_back(i);
-    }
-//    Solution* test = new Solution();
-//    test->search()
-    Solution test;
-    cout<<test.search(obj,1);
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
